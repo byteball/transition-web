7b045e453205be8d053959addd012309f2707a75
tony <tonyofbyteball@users.noreply.github.com>
2016-12-07 21:27:03 +0300

    is_active, order

--- index.js
+++ index.js
@@ -21,12 +21,19 @@ http.createServer(function(request, response) {
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write(file, "binary");
 
-		db.all("SELECT 'tx' AS type, proof_id AS id, byteball_address, bitcoin_address, balance, 0 AS bytes, txid AS tx_sig FROM proof_transactions \
-			UNION SELECT 'signature', signed_message_id, byteball_address, bitcoin_address, balance, 0 AS bytes, signature FROM signed_messages", function(err, rows) {
-			response.write('<script type="text/javascript"> var dataJSON = ' + JSON.stringify(rows) + '</script>');
-			response.write('</body></html>');
-			response.end();
-		});
+		db.all(
+			"SELECT 'tx' AS type, proof_id AS id, byteball_address, bitcoin_address, balance, 0 AS bytes, txid AS tx_sig \
+			FROM proof_transactions WHERE is_active=1 \
+			UNION \
+			SELECT 'signature' AS type, signed_message_id AS id, byteball_address, bitcoin_address, balance, 0 AS bytes, signature AS tx_sig \
+			FROM signed_messages WHERE is_active=1 \
+			ORDER BY creation_date DESC", 
+			function(err, rows) {
+				response.write('<script type="text/javascript"> var dataJSON = ' + JSON.stringify(rows) + '</script>');
+				response.write('</body></html>');
+				response.end();
+			}
+		);
 	});
 }).listen(parseInt(port, 10));
 
